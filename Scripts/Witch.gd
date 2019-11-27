c2137260dea3bde83eebf0be7c8d22aafde03180
Rachel Gentry <rakw1995@gmail.com>
2019-11-26 22:28:07 -0500

    makes changes

--- Scripts/Witch.gd
+++ Scripts/Witch.gd
@@ -4,13 +4,14 @@ onready var camera = $Pivot/Camera
 
 var gravity = -30
 var max_speed = 8
-var mouse_sensitivity = 0.002  # radians/pixel
+var mouse_sensitivity = 0.002
 
 var velocity = Vector3()
 
+var Bullet = preload("res://Scenes/Bullet.tscn")
+
 func get_input():
     var input_dir = Vector3()
-    # desired move in camera direction
     if Input.is_action_pressed("move_forward"):
         input_dir += -camera.global_transform.basis.z
     if Input.is_action_pressed("move_back"):
@@ -21,12 +22,19 @@ func get_input():
         input_dir += camera.global_transform.basis.x
     input_dir = input_dir.normalized()
     return input_dir
+	
 
 func _unhandled_input(event):
     if event is InputEventMouseMotion:
         rotate_y(-event.relative.x * mouse_sensitivity)
         $Pivot.rotate_x(-event.relative.y * mouse_sensitivity)
         $Pivot.rotation.x = clamp($Pivot.rotation.x, -1.2, 1.2)
+
+    if event.is_action_pressed("attack"):
+        var b = Bullet.instance()
+        b.start($FB.global_transform)
+        get_parent().add_child(b)
+        
 
 func _physics_process(delta):
     velocity.y += gravity * delta
@@ -35,3 +43,4 @@ func _physics_process(delta):
     velocity.x = desired_velocity.x
     velocity.z = desired_velocity.z
     velocity = move_and_slide(velocity, Vector3.UP, true)
+	
